57c52a6275164fd2e10b5f7f2f31860a3056d39e
Grabacr07 <grabacr07+github@gmail.com>
2015-07-26 05:48:09 +0900

    TabView の IsSelected の実装を変更

--- MetroRadiance/Controls/TabView.cs
+++ MetroRadiance/Controls/TabView.cs
@@ -14,5 +14,19 @@ namespace MetroRadiance.Controls
 		{
 			DefaultStyleKeyProperty.OverrideMetadata(typeof(TabView), new FrameworkPropertyMetadata(typeof(TabView)));
 		}
+
+		protected override void OnSelectionChanged(SelectionChangedEventArgs e)
+		{
+			base.OnSelectionChanged(e);
+
+			foreach (var item in e.RemovedItems.OfType<ITabItem>())
+			{
+				item.IsSelected = false;
+			}
+			foreach (var item in e.AddedItems.OfType<ITabItem>())
+			{
+				item.IsSelected = true;
+			}
+		}
 	}
 }
