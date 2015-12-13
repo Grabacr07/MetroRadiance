768d13d563fe67549150316dcbdfff0f14d95e3f
Grabacr07 <grabacr07+github@gmail.com>
2015-12-14 07:45:45 +0900

    バージョン調整

--- source/MetroRadiance/Properties/AssemblyInfo.cs
+++ source/MetroRadiance/Properties/AssemblyInfo.cs
@@ -47,5 +47,5 @@ using System.Windows.Markup;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha")]
+[assembly: AssemblyVersion("2.0.1")]
+[assembly: AssemblyInformationalVersion("2.0.1-alpha")]
