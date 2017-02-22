a13c691e8946d9e1b1d6a1062569e32648ec95cd
Grabacr07 <grabacr07+github@gmail.com>
2017-02-23 03:36:44 +0900

    v2.4

--- source/MetroRadiance.Core/Properties/AssemblyInfo.cs
+++ source/MetroRadiance.Core/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Windows;
 	ResourceDictionaryLocation.None,
 	ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("2.2.1")]
-[assembly: AssemblyInformationalVersion("2.2.1")]
+[assembly: AssemblyVersion("2.4.0")]
+[assembly: AssemblyInformationalVersion("2.4.0")]
